aaa5aa040f87ce647134015187dcf422b14b56aa
Hans Hübner <hans.huebner@gmail.com>
2025-02-13 06:46:58 +0100

    remove oldschool files

--- backend/src/paths.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-import fs from 'fs'
-
-export const resolvePath = (...components: string[]) =>
-  path.join(__dirname, '../..', ...components)
-
-export const readFileSync = (...components: string[]) =>
-  fs.readFileSync(resolvePath(...components), { encoding: 'utf8' })
--- backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './app'
-
-const port = parseInt(process.env.PORT || '3000')
-
-app.listen(port, async () => {
-  console.log(`Server running on port ${port}`)
-})
