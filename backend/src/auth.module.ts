4cc014a2a5804a4a18f24a11e4b479c96ba5f1dc
Hans Hübner <hans.huebner@gmail.com>
2025-02-13 08:03:24 +0100

    remove clunky backend

--- backend/src/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { GoogleStrategy } from './strategies/google.strategy';
-
-@Module({
-  imports: [PassportModule.register({ defaultStrategy: 'google' })],
-  providers: [GoogleStrategy],
-})
-export class AuthModule {}
