35d25ed263715e1b8a750af5dc9f1c72949bdb51
Hans Hübner <hans.huebner@gmail.com>
2025-02-21 08:29:59 +0100

    fix default session secret

--- backend/src/app/session.ts
+++ backend/src/app/session.ts
@@ -3,7 +3,18 @@ import fastifySession from '@fastify/session'
 import { FastifyInstance } from 'fastify'
 
 export const register = (app: FastifyInstance) => {
+  const getSessionSecret = () => {
+    const secret = process.env.SESSION_SECRET
+    if (secret) {
+      return secret
+    }
+    app.log.warn(
+      'SESSION_SECRET environment variable not set, returning preconfigured secret',
+    )
+    return 'oi31p2oi312po3i12p3n21be jn)@!(#d432bhjdbhbehbHJKHJG'
+  }
+
   app.register(fastifySession, {
-    secret: process.env.SESSION_SECRET || 'dnewndqwewjdnqw43',
+    secret: getSessionSecret(),
   })
 }
