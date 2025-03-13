fabe76569f54a7f1ad7924e69102e9f7bd91bb71
Hans Hübner <hans.huebner@gmail.com>
2025-03-13 05:41:09 +0100

    include combined graphql schema file in build output

--- backend/src/app/graphql.ts
+++ backend/src/app/graphql.ts
@@ -8,16 +8,13 @@ import { createContext, destroyContext } from './context.js'
 import { RequestContext } from '@mikro-orm/core'
 import { fileURLToPath } from 'node:url'
 import { initORM } from '../db.js'
-import { mergeTypeDefs } from '@graphql-tools/merge'
-import { glob } from 'glob'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// Read and merge all schema files
-const schemaFiles = glob.sync(path.join(__dirname, '../**/*.graphql'))
-const typeDefs = mergeTypeDefs(
-  schemaFiles.map((file) => readFileSync(file, { encoding: 'utf-8' })),
+const typeDefs = readFileSync(
+  path.join(__dirname, '../generated/combined-schema.graphql'),
+  { encoding: 'utf-8' },
 )
 
 const createServer = async () =>
