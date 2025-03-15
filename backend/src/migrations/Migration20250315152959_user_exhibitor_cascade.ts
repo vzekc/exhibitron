a9720d7db378ff37249e6a877f4de85e505a6b6d
Hans Hübner <hans.huebner@gmail.com>
2025-03-15 15:31:58 +0100

    add another missing cascade constraint

---
+++ backend/src/migrations/Migration20250315152959_user_exhibitor_cascade.ts
@@ -0,0 +1,17 @@
+import { Migration } from '@mikro-orm/migrations'
+
+export class Migration20250315152959_user_exhibitor_cascade extends Migration {
+  async up(): Promise<void> {
+    this.addSql('ALTER TABLE "exhibitor" DROP CONSTRAINT "exhibitor_user_id_foreign";')
+    this.addSql(
+      'ALTER TABLE "exhibitor" ADD CONSTRAINT "exhibitor_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON UPDATE CASCADE ON DELETE CASCADE;',
+    )
+  }
+
+  async down(): Promise<void> {
+    this.addSql('ALTER TABLE "exhibitor" DROP CONSTRAINT "exhibitor_user_id_foreign";')
+    this.addSql(
+      'ALTER TABLE "exhibitor" ADD CONSTRAINT "exhibitor_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON UPDATE CASCADE;',
+    )
+  }
+} 
