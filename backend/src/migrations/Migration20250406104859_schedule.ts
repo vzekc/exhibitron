8600dc53d45ad5543033689aa1f4d598f7c3a3e8
Hans Hübner <hans.huebner@gmail.com>
2025-04-06 12:52:30 +0200

    migration for schedule models

---
+++ backend/src/migrations/Migration20250406104859_schedule.ts
@@ -0,0 +1,64 @@
+import { Migration } from '@mikro-orm/migrations'
+
+export class Migration20250406104859_schedule extends Migration {
+  override async up(): Promise<void> {
+    this.addSql(
+      `create table "room" ("id" serial primary key, "created_at" timestamptz not null, "updated_at" timestamptz null, "name" varchar(255) not null, "capacity" int null, "exhibition_id" int not null);`,
+    )
+
+    this.addSql(
+      `create table "presentation" ("id" serial primary key, "created_at" timestamptz not null, "updated_at" timestamptz null, "title" varchar(255) not null, "start_time" timestamptz null, "end_time" timestamptz null, "exhibition_id" int not null, "room_id" int null, "description_id" int null);`,
+    )
+
+    this.addSql(
+      `create table "presentation_exhibitors" ("presentation_id" int not null, "exhibitor_id" int not null, constraint "presentation_exhibitors_pkey" primary key ("presentation_id", "exhibitor_id"));`,
+    )
+
+    this.addSql(
+      `alter table "room" add constraint "room_exhibition_id_foreign" foreign key ("exhibition_id") references "exhibition" ("id") on update cascade;`,
+    )
+
+    this.addSql(
+      `alter table "presentation" add constraint "presentation_exhibition_id_foreign" foreign key ("exhibition_id") references "exhibition" ("id") on update cascade;`,
+    )
+    this.addSql(
+      `alter table "presentation" add constraint "presentation_room_id_foreign" foreign key ("room_id") references "room" ("id") on update cascade on delete set null;`,
+    )
+    this.addSql(
+      `alter table "presentation" add constraint "presentation_description_id_foreign" foreign key ("description_id") references "document" ("id") on update cascade on delete set null;`,
+    )
+
+    this.addSql(
+      `alter table "presentation_exhibitors" add constraint "presentation_exhibitors_presentation_id_foreign" foreign key ("presentation_id") references "presentation" ("id") on update cascade on delete cascade;`,
+    )
+    this.addSql(
+      `alter table "presentation_exhibitors" add constraint "presentation_exhibitors_exhibitor_id_foreign" foreign key ("exhibitor_id") references "exhibitor" ("id") on update cascade on delete cascade;`,
+    )
+
+    this.addSql(
+      `alter table "exhibition" add column "start_date" timestamptz null, add column "end_date" timestamptz null;`,
+    )
+    this.addSql(
+      `update "exhibition" set "start_date" = '2025-09-11 00:00:00+00', "end_date" = '2025-09-14 23:59:59+00';`,
+    )
+    this.addSql(
+      `alter table "exhibition" alter column "start_date" set not null, alter column "end_date" set not null;`,
+    )
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`alter table "presentation" drop constraint "presentation_room_id_foreign";`)
+
+    this.addSql(
+      `alter table "presentation_exhibitors" drop constraint "presentation_exhibitors_presentation_id_foreign";`,
+    )
+
+    this.addSql(`drop table if exists "room" cascade;`)
+
+    this.addSql(`drop table if exists "presentation" cascade;`)
+
+    this.addSql(`drop table if exists "presentation_exhibitors" cascade;`)
+
+    this.addSql(`alter table "exhibition" drop column "start_date", drop column "end_date";`)
+  }
+}
