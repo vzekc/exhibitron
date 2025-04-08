445254f0040b41fa0e4cbb39a73d26d505f38027
Hans Hübner <hans.huebner@gmail.com>
2025-04-08 21:02:56 +0200

    Build-Timestamp nach deutschen Regeln formatieren
    Fixes #61

--- frontend/src/components/BuildInfo.tsx
+++ frontend/src/components/BuildInfo.tsx
@@ -9,7 +9,16 @@ const BuildInfo = () => (
       <div>{buildInfo.buildMode}</div>
       <div>{buildInfo.branchName}</div>
       <div>{buildInfo.commitSha}</div>
-      <div className="whitespace-nowrap">{new Date(buildInfo.deploymentDate).toLocaleString()}</div>
+      <div className="whitespace-nowrap">
+        {new Date(buildInfo.deploymentDate).toLocaleString('de-DE', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })}
+      </div>
     </div>
   </div>
 )
